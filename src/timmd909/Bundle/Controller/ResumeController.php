2aee45872a07a0ba93262934e1e4ecdb7f4e63c3
Tim Doerzbacher <tim@tim-doerzbacher.com>
2014-03-28 10:01:38 -0400

    Removing vestigial commented code

--- src/timmd909/Bundle/Controller/ResumeController.php
+++ src/timmd909/Bundle/Controller/ResumeController.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @author Tim Doerzbacher <tim@tim-doerzbacher.com>
- * 
  */
 
 namespace timmd909\Bundle\Controller;
@@ -13,42 +12,18 @@ class ResumeController extends ControllerBase
     public function resumeAction()
     {
         $options = array(
-            'page' => 'resume',  
-            'pages' => $this->getDefaultPages() 
+            'page' => 'resume',
+            'pages' => $this->getDefaultPages()
         );
         $template = $this->getTemplateFilename('resume');
-		
+
 		$options['experiences']       = $this->experienceRepository->findAll();
 		$options['skills']            = $this->skillRepository->findAll();
 		$options['tools']             = $this->toolRepository->findAll();
 		$options['languages']         = $this->languageRepository->findAll();
 		$options['operating_systems'] = $this->operatingSystemRepository->findAll();
-		
-		
-		// // load all the categories from the database
-		// $linkCategories = $this->linkCategoryRepository->findAll(array('weight'=>'asc'));
-		// $options['links'] = array();
-// 		
-		// foreach ($linkCategories as $category) {
-			// $tempCategory = array(
-				// 'name'  => $category->getName(),
-				// 'links' => array()
-			// );
-// 			
-			// foreach ($category->getLinks() as $link) {
-				// $tempLink = array(
-					// 'name'        => $link->getName(),
-					// 'href'        => $link->getTarget(),
-					// 'description' => $link->getDescription(),
-					// 'background'  => $link->getImage()
-				// );
-				// $tempCategory['links'][] = $tempLink;
-			// }
-// 			
-			// $options['links'][] = $tempCategory;
-		// }
-		
+
 		return $this->render($template, $options);
 	}
-	    
+
 }
