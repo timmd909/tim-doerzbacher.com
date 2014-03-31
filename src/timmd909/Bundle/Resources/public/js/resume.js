0a30f61a6d4bc5ac2f75b3d0beb7b5cd60526319
Tim Doerzbacher <tim@tim-doerzbacher.com>
2014-03-31 03:40:04 -0400

    Finishing up the resume….
    well, for now at least

--- src/timmd909/Bundle/Resources/public/js/resume.js
+++ src/timmd909/Bundle/Resources/public/js/resume.js
@@ -5,4 +5,13 @@ $(function() {
 		columnWidth: 300, 
 		itemSelector: '.sub-section'
 	});
+
+	$('.contact-info .container').masonry({
+		columnWidth: 300, 
+		itemSelector: 'div'
+	});
+	
+	
+	$('.spam-prevention').remove();
+	$('.email-address .domain').html('tim-doerzbacher.com');
 });
