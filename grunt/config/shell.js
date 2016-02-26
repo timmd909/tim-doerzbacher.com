9d96adff22ae75e5463cb7c457d3e6f5c30dffb1
Tim Doerzbacher <tim@tim-doerzbacher.com>
2016-02-25 23:31:33 -0500

    Tweaks for better wkhtmltopdf to work better

--- grunt/config/shell.js
+++ grunt/config/shell.js
@@ -1,8 +1,15 @@
 'use strict';
 
+var WKHTMLTOPDF_OPTIONS = [
+	'--print-media-type',
+	'--no-background',
+	'--page-width 8.5in',
+	'--page-height 11in'
+].join(' ');
+
 module.exports = {
 	'resume': {
-		'command': 'wkhtmltopdf --print-media-type app/cache/resume/index.html web/resume.pdf || true'
+		'command': 'wkhtmltopdf ' + WKHTMLTOPDF_OPTIONS + ' app/cache/resume/index.html web/resume.pdf || true'
 	}
 };
 
