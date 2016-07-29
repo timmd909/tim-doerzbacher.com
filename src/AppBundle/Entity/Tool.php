49fccd214e8d148d370fc2bf56052133c96a38f3
Tim Doerzbacher <tim@tim-doerzbacher.com>
2016-07-28 21:29:09 -0400

    Fix skills and tools bug
    Hmmm... inheritance seems a bit funky... I'll come back to this

--- src/AppBundle/Entity/Tool.php
+++ src/AppBundle/Entity/Tool.php
@@ -10,6 +10,84 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="tools")
  */
-class Tool extends Skill
+class Tool
 {
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $name;
+
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $level;
+
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/* -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- */
+
+	/**
+	 * Set name
+	 *
+	 * @param string $name
+	 * @return Language
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Get name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/* -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- -=- */
+
+	/**
+	 * Set level
+	 *
+	 * @param string $level
+	 * @return Language
+	 */
+	public function setLevel($level)
+	{
+		$this->level = $level;
+
+		return $this;
+	}
+
+	/**
+	 * Get level
+	 *
+	 * @return string
+	 */
+	public function getLevel()
+	{
+		return $this->level;
+	}
 }
